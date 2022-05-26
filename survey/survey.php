eee1dcad40fd2841a13bb393e8f4844b805d003d
Mrbelekas <mantas321nauronis@gmail.com>
2022-05-26 13:56:35 +0300

    completed survey

--- survey/survey.php
+++ survey/survey.php
@@ -10,26 +10,48 @@
             include "../nav/footer.php";
         ?>
         <br>
-       <form action="survey.cgi" method="POST">
+       <form method="post">
  
         <lable for="fname">Please enter your first name: </lable><br> 
-        <input type="text" name="fname" id="fname" size=35 maxlength=50 value="">
+        <input type="text" name="fname" id="fname" size=35 maxlength=50 value="" required>
         <br>
         <lable for="lname">Please enter your last name: </lable><br>  
-        <input type="text" name="lname" id="lname" size=35 maxlength=50 value="">
+        <input type="text" name="lname" id="lname" size=35 maxlength=50 value="" required>
         <br>
         <labal for="age">Please enter your age: </lable><br>   
-        <input type="number" name="age" id="age" min=18 max=120 value="">
+        <input type="number" name="age" id="age" min=18 max=120 value="" required>
         <p>Please choose your sex:</p>
-        <input type="radio" id="male" name="sex" value="Male">
+        <input type="radio" id="male" name="sex" value="Male" required>
         <label for="male">Male</label><br>
-        <input type="radio" id="female" name="sex" value="Female">
+        <input type="radio" id="female" name="sex" value="Female" required>
         <label for="female">Female</label><br>
-        <input type="radio" id="else" name="sex" value="Else">
+        <input type="radio" id="else" name="sex" value="Else" required>
         <label for="else">Else</label><br>
         <label for="email">Please enter your email:</label><br>
-        <input type="email" id="email" name="email"><br>
-        <input type="submit" value="Submit">
+        <input type="email" id="email" name="email"required><br>
+        <input type="submit" value="submit" name="submit" id="submit">
         </form>
-    </body>
+            
 </html>
+<?php
+    if (isset($_POST['submit'])){
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $age = $_POST["age"];
+        $sex = $_POST["sex"];
+        $email = $_POST["email"];
+        $file_name = $fname . "_file.txt";
+
+        $openFile = fopen($file_name, 'a');
+            $data = "\t"."{$fname}";
+            $data .= "\t"."{$lname}";
+            $data .= "\t"."{$age}";
+            $data .= "\t"."{$sex}";
+            $data .= "\t"."{$email}";
+            $data .= "\n"; 
+
+        fwrite($openFile,$data);
+        fclose($openFile);
+    }
+?>
+    </body>
